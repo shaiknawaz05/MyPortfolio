14e23b8ca7fa21f65afd3b2235a54c5132581763
shaiknawaz05 <shaiknawaz05@gmail.com>
2020-12-13 22:00:42 +0530

    init
    Signed-off-by: shaiknawaz05 <shaiknawaz05@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import MyNavbar from "./components/my-navbar/mynavbar.component";
-
+import MyCarousel from "./components/my-carousel/my-carousel.component";
+import TitleMessage from "./components/title-message/title-message.component"
 
 import "./App.css";
 
 const App = () => {
   return (<div>
       <MyNavbar />
-      My
+      <MyCarousel/>
+      <TitleMessage/>
     </div>
   );
 };

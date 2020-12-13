c110740f4b016a6f5f7264022c51ce4e2ae1f945
shaiknawaz05 <shaiknawaz05@gmail.com>
2020-12-13 11:22:46 +0530

    initial commit added mynavbar component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+
+
+import "./App.css";
+
+const App = () => {
+  return (<div>
+      <MyNavbar />
+      My
+    </div>
+  );
+};
+
+export default App;
---
+++ src/themes/theme.js
@@ -0,0 +1,5 @@
+export const theme = {
+    primary: "#0b0c10",
+    textColor: "#66fcf1"
+  };
+  

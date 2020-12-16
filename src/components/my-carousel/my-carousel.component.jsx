42d53cf37327b61b91ff2fa00c70e4ae4f5ae108
shaiknawaz05 <shaiknawaz05@gmail.com>
2020-12-16 16:49:03 +0530

    init
    Signed-off-by: shaiknawaz05 <shaiknawaz05@gmail.com>

--- src/components/my-carousel/my-carousel.component.jsx
+++ src/components/my-carousel/my-carousel.component.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import slide1 from '../../assets/img/carousal/slide1.jpg'
-import slide2 from '../../assets/img/carousal/slide2.jpg'
 import slide3 from '../../assets/img/carousal/slide3.jpg'
 import ScrollDown from "../scroll-down/scroll-down.component";
 import './my-carousel.style.css'
@@ -9,15 +7,9 @@ import './my-carousel.style.css'
 const MyCarousel = () => {
     return (
       <div id="home">
-      <Carousel controls={false} indicators interval={2500} pause={false}>
-        <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={slide2} alt="First slide" />
-        </Carousel.Item>
+      <Carousel controls={false} indicators interval={2500} pause={false}>   
         <Carousel.Item>
           <img className="d-block w-100 custom-img" src={slide3} alt="Third slide" />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100 custom-img" src={slide1} alt="Third slide" />
         </Carousel.Item>
       </Carousel>
       <ScrollDown />

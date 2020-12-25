17da6dd9429db473a20ab3425980ad6e3f42c852
shaiknawaz05 <shaiknawaz05@gmail.com>
2020-12-25 13:19:50 +0530

    commit
    Signed-off-by: shaiknawaz05 <shaiknawaz05@gmail.com>

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -40,7 +40,7 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://drive.google.com/file/d/1m6rxpSFpMLuKkEool70N7ojpJcI5FiUU/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1gOWmXhZ7XwIB_8gtLkCjylG-jXXNSvn1/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
